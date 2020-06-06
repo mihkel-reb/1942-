c01d293bba26ff81959f352fb36b2b6950d3b9fa
cududont <45621973+cududont@users.noreply.github.com>
2020-06-07 01:20:50 +0300

    Add files via upload

--- beter 1942.py
+++ beter 1942.py
@@ -2,12 +2,7 @@ import turtle
 import random
 import sys
 import os
-
-wn = turtle.Screen()
-wn.bgcolor("black")
-wn.setup(width=500, height=600)
-wn.bgcolor("lightblue")
-wn.tracer(0)
+from time import sleep
 
 path = os.getcwd()
 
@@ -16,15 +11,32 @@ pMissiles = []
 eMissiles = []
 score = 0
 bEnemylives = 3
+gOver = False
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.setup(width=500, height=600)
+wn.bgpic("{}/resources/bg.gif".format(path))
+wn.tracer(0)
+
 
 pPlane = "{}/resources/plane.gif".format(path)
 sEnemyplane = "{}/resources/senemyplane.gif".format(path)
 bEnemyplane = "{}/resources/benemyplane.gif".format(path)
+pMissilepic = "{}/resources/playermissile.gif".format(path)
+eMissilepic = "{}/resources/enemymissile.gif".format(path)
 
 wn.addshape(pPlane)
 wn.addshape(sEnemyplane)
 wn.addshape(bEnemyplane)
+wn.addshape(pMissilepic)
+wn.addshape(eMissilepic)
 
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.penup()
+pen.setpos(0, 270)
+pen.write("Score: " + str(score), move=False, align="center", font=("Comic Sans MS", 12)) 
 
 player = turtle.Turtle()
 player.shape(pPlane)
@@ -47,8 +59,7 @@ for i in range(3):
     pMissile = turtle.Turtle()
     pMissile.penup()
     pMissile.setpos(1000, 1000)
-    pMissile.color("yellow")
-    pMissile.shape("square")
+    pMissile.shape(pMissilepic)
     pMissile.turtlesize(0.3)
     pMissile.state = "ready"
     pMissiles.append(pMissile)
@@ -57,7 +68,7 @@ for i in range(3):
     eMissile = turtle.Turtle()
     eMissile.hideturtle()
     eMissile.color("orange")
-    eMissile.shape("square")
+    eMissile.shape(eMissilepic)
     eMissile.turtlesize(0.4)
     eMissile.penup()
     eMissile.state = "ready"
@@ -101,7 +112,8 @@ def fireeMissile():
             eMissile.showturtle()
             eMissile.state = "fire"
             break
-    
+
+
 wn.listen()
 wn.onkeypress(left, "Left")
 wn.onkeypress(right, "Right")
@@ -114,16 +126,16 @@ while True:
     wn.update()
 
     if player.direction == "left" and player.xcor() > -230:
-        player.setx(player.xcor() - 0.6)
+        player.setx(player.xcor() - 0.7)
     
     elif player.direction == "right" and player.xcor() < 230:
-        player.setx(player.xcor() + 0.6)
+        player.setx(player.xcor() + 0.7)
     
     elif player.direction == "up" and player.ycor() < 270:
-        player.sety(player.ycor() + 0.6)
+        player.sety(player.ycor() + 0.7)
     
     elif player.direction == "down" and player.ycor() > -270:
-        player.sety(player.ycor() - 0.6)
+        player.sety(player.ycor() - 0.7)
 
     for pMissile in pMissiles:
         if pMissile.state == "fire":
@@ -138,29 +150,32 @@ while True:
 
     for eMissile in eMissiles:
         if eMissile.state == "fire":
-            eMissile.sety(eMissile.ycor() - 3)
+            eMissile.sety(eMissile.ycor() - 2.5)
 
         if eMissile.xcor() > 250 or eMissile.xcor() < -250 or eMissile.ycor() > 300 or eMissile.ycor() < -300:
             eMissile.hideturtle()
             eMissile.state = "ready"
 
     for sEnemy in sEnemies:    
-        #sEnemy.sety(sEnemy.ycor() - random.uniform(0.5, 1.3))
+        sEnemy.sety(sEnemy.ycor() - random.uniform(0.5, 1.3))
         sEnemy.setx(sEnemy.xcor() + random.uniform(-2, 2))
     
         if sEnemy.ycor() < -300:
             sEnemy.goto(random.randint(-300, 300), random.randint(400, 800))
     
         if player.distance(sEnemy) < 25:
-            sys.exit(0)
-
+            gOver = True
+            
+            
         for pMissile in pMissiles:
             if pMissile.distance(sEnemy) < 10:
                 sEnemy.hideturtle()
                 sEnemy.goto(random.randint(-250, 250), random.randint(400, 800))
                 sEnemy.showturtle()
                 score += 10
-                print(score) 
+                pen.clear()
+                pen.write("Score: " + str(score), move=False, align="center", font=("Comic Sans MS", 12)) 
+                
 
                 pMissile.setpos(1000, 1000)
                 pMissile.hideturtle()
@@ -170,13 +185,15 @@ while True:
     bEnemy.setx(bEnemy.xcor() + random.uniform(-1, 1))
 
     if player.distance(bEnemy) < 40:
-            sys.exit(0)
+        gOver = True
+            
 
     if bEnemy.ycor() < -300:
         bEnemy.goto(random.randint(-220, 220), random.randint(400, 800))
+        bEnemylives = 3
 
     for pMissile in pMissiles:
-        if pMissile.distance(bEnemy) < 30:
+        if pMissile.distance(bEnemy) < 35:
             bEnemylives -= 1
             print(bEnemylives)
             if bEnemylives > 0:
@@ -188,6 +205,8 @@ while True:
                 bEnemylives = 3
                 print(bEnemylives)
                 score += 30
+                pen.clear()
+                pen.write("Score: " + str(score), move=False, align="center", font=("Comic Sans MS", 12))
                 print(score)
             
             pMissile.setpos(1000, 1000)
@@ -196,7 +215,37 @@ while True:
 
     for eMissile in eMissiles:
         if eMissile.distance(player) < 10:
-            sys.exit(0)
+            gOver = True
+
+    if gOver:
+        pen.setpos(0, 0)
+        pen.write("Game Over!", move=False, align="center", font=("Comic Sans MS", 24))
+        pen.setpos(0, -25)
+        pen.write("Your Score Was: " + str(score), move=False, align="center", font=("Comic Sans MS", 16))
+        pen.setpos(0, 0)
+
+        bEnemy.setpos(random.randint(-220, 220), 250)
+        for sEnemy in sEnemies:
+            sEnemy.goto(random.randint(-300, 300), random.randint(400, 800))
+
+        for eMissile in eMissiles:
+            eMissile.setpos(1000, 1000)
+
+        for pMissile in pMissiles:
+            pMissile.setpos(1000, 100)
+        
+        player.direction = "stop"
+        player.goto(0, -250)
+        
+        sleep(2)
+        score = 0
+        pen.clear()
+        pen.setpos(0, 270)
+        pen.write("Score: " + str(score), move=False, align="center", font=("Comic Sans MS", 12)) 
+        gOver = False
+        
+      
+
     
     
 
